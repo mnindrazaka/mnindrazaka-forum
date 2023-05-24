1126fdd270350a2d266fba4a5dc24d40b421424a
M. Nindra Zaka <mnindrazaka@gmail.com>
2023-05-24 14:22:21 +0700

    fix: storybook

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,35 +1,6 @@
-import React from 'react';
-import { Button as TamaguiButton } from 'tamagui'
-import './button.css';
+import React from "react";
+import { Button as TamaguiButton } from "tamagui";
 
-interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
-}
-
-/**
- * Primary UI component for user interaction
- */
 export const Button = () => {
-  return (
-    <TamaguiButton>Hello</TamaguiButton>
-  );
+  return <TamaguiButton>Hello</TamaguiButton>;
 };
