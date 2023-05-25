1372c876a0d1b5a778ef4bec6ebbc37f041b9311
M. Nindra Zaka <mnindrazaka@gmail.com>
2023-05-25 16:53:22 +0700

    feat: add PostListScreen to route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,2 @@
-import { Button, YStack } from "tamagui";
-
-export default function Home() {
-  return (
-    <YStack>
-      <Button>Press Me</Button>
-    </YStack>
-  );
-}
+import { PostListScreen } from "@/posts/screens";
+export default PostListScreen;
