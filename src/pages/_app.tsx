a3144ed97dc8d96f9fb8448a5b227ac94c2bbbf6
M. Nindra Zaka <mnindrazaka@gmail.com>
2023-05-29 14:06:52 +0700

    chore: change project name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,7 +29,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Karirlab Forum</title>
+        <title>M. Nindra Zaka Forum</title>
       </Head>
       <Script
         id="tamagui-animations-mount"
