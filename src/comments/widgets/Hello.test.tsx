7995971ded1108290fde13debe3b4ac7deae5ffa
M. Nindra Zaka <mnindrazaka@gmail.com>
2023-05-29 08:38:35 +0700

    refactor: remove unused component

--- src/comments/widgets/Hello.test.tsx
+++
@@ -1,7 +0,0 @@
-import { Hello } from "./Hello";
-import { render, screen } from "@testing-library/react";
-
-test("loads and displays greeting", async () => {
-  render(<Hello />);
-  expect(screen.getByText("Hello")).toBeInTheDocument();
-});
